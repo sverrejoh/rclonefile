7e64dd86317271bb0e9b0e2a0c232983c6cb0276
Sverre Johansen <svjohan@microsoft.com>
2023-02-13 17:01:26 +0100

    Adding arguments.

--- src/lib.rs
+++ src/lib.rs
@@ -10,17 +10,59 @@ use std::ffi::CString;
 #[macro_use]
 extern crate napi_derive;
 
+#[napi(object)]
+pub struct CloneFileOptions {
+  pub no_follow: bool,
+  pub no_owner_copy: bool,
+  pub clone_acl: bool,
+}
+
+impl Default for CloneFileOptions {
+  fn default() -> Self {
+    CloneFileOptions {
+      no_follow: false,
+      no_owner_copy: false,
+      clone_acl: false,
+    }
+  }
+}
+
+const CLONE_NOFOLLOW: u32 = 1 << 0;
+const CLONE_NOOWNERCOPY: u32 = 1 << 1;
+const CLONE_ACL: u32 = 1 << 2;
+
+fn flags_from_options(options: Option<CloneFileOptions>) -> u32 {
+  match options {
+    None => 0,
+    Some(options) => {
+      let CloneFileOptions {
+        no_follow,
+        no_owner_copy,
+        clone_acl,
+      } = options;
+
+      let flags = if no_follow { CLONE_NOFOLLOW } else { 0 }
+        | if no_owner_copy { CLONE_NOOWNERCOPY } else { 0 }
+        | if clone_acl { CLONE_ACL } else { 0 };
+
+      flags
+    }
+  }
+}
+
 #[napi(js_name = "cloneFileSync")]
-pub fn clonefile_sync(src: String, dst: String) -> Result<i32> {
-  let src = CString::new(src).unwrap();
-  let dst = CString::new(dst).unwrap();
-  let res = unsafe { clonefile(src.as_ptr(), dst.as_ptr(), 0) };
+pub fn clonefile_sync(src: String, dst: String, options: Option<CloneFileOptions>) -> Result<i32> {
+  let src = CString::new(src)?;
+  let dst = CString::new(dst)?;
+  let flags = flags_from_options(options);
+
+  let res = unsafe { clonefile(src.as_ptr(), dst.as_ptr(), flags) };
 
   if res == -1 {
     let e = errno();
     set_errno(e);
     let code = e.0;
-    return Err(Error::from_reason(format!("{code} {e}")));
+    return Err(Error::from_reason(format!("{code}: {e}")));
   } else {
     return Ok(res);
   }
@@ -37,8 +79,7 @@ impl Task for AsyncClonefile {
   type JsValue = JsNumber;
 
   fn compute(&mut self) -> Result<Self::Output> {
-    let res = clonefile_sync(self.src.clone(), self.dst.clone());
-    return res;
+    clonefile_sync(self.src.clone(), self.dst.clone(), Default::default())
   }
 
   fn resolve(&mut self, env: Env, output: i32) -> Result<Self::JsValue> {
