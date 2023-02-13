80057b21ac00d9514740ec443fb5beb72f51467c
Sverre Johansen <svjohan@microsoft.com>
2023-02-13 20:33:31 +0100

    Add options + tests

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,4 +1,5 @@
 import { describe, expect, test } from "@jest/globals";
+import { promises as fs, existsSync } from "fs";
 import { cloneFile, cloneFileSync } from "../index";
 import { directory } from "tempy";
 import { join } from "path";
@@ -7,10 +8,13 @@ import { readFileSync } from "fs";
 describe("cloneFile", () => {
   test("clone file sync", () => {
     const tempDir = directory();
+    const source = join(__dirname, "inputs/mario.txt");
     const target = join(tempDir, "mario-clone.txt");
-    cloneFileSync(join(__dirname, "inputs/mario.txt"), target);
+    cloneFileSync(source, target);
 
-    const sourceData = readFileSync(target).toString();
+    expect(existsSync(target)).toBe(true);
+
+    const sourceData = readFileSync(source).toString();
     const targetData = readFileSync(target).toString();
 
     expect(sourceData).toBe(targetData);
@@ -18,12 +22,74 @@ describe("cloneFile", () => {
 
   test("clone file async", async () => {
     const tempDir = directory();
+    const source = join(__dirname, "inputs/mario.txt");
     const target = join(tempDir, "mario-clone.txt");
-    const result = await cloneFile(join(__dirname, "inputs/mario.txt"), target);
+    const result = await cloneFile(source, target);
 
-    const sourceData = readFileSync(target).toString();
+    expect(existsSync(target)).toBe(true);
+
+    const sourceData = readFileSync(source).toString();
+    const targetData = readFileSync(target).toString();
+    expect(sourceData).toBe(targetData);
+  });
+
+  test("clonefile async link follows and copies target", async () => {
+    const tempDir = directory();
+    const source = join(__dirname, "inputs/mario-link.txt");
+    const target = join(tempDir, "mario-clone.txt");
+    const result = await cloneFile(source, target);
+
+    const sourceData = readFileSync(source).toString();
     const targetData = readFileSync(target).toString();
 
     expect(sourceData).toBe(targetData);
+
+    expect(existsSync(target)).toBe(true);
+
+    const stat = await fs.lstat(target);
+    expect(stat.isFile()).toBe(true);
+    expect(stat.isSymbolicLink()).toBe(false);
+  });
+
+  test("clonefile async ignores link when no-follow: true", async () => {
+    const tempDir = directory();
+    const source = join(__dirname, "inputs/mario-link.txt");
+    const target = join(tempDir, "mario-clone.txt");
+    const result = await cloneFile(source, target, { noFollow: true });
+
+    const stat = await fs.lstat(target);
+
+    expect(existsSync(target)).toBe(false);
+    expect(stat.isSymbolicLink()).toBe(true);
+  });
+
+  test("clonefile sync link follows and copies target", async () => {
+    const tempDir = directory();
+    const source = join(__dirname, "inputs/mario-link.txt");
+    const target = join(tempDir, "mario-clone.txt");
+    const result = cloneFileSync(source, target);
+
+    const sourceData = readFileSync(source).toString();
+    const targetData = readFileSync(target).toString();
+
+    expect(sourceData).toBe(targetData);
+
+    expect(existsSync(target)).toBe(true);
+
+    const stat = await fs.lstat(target);
+    expect(stat.isFile()).toBe(true);
+    expect(stat.isSymbolicLink()).toBe(false);
+  });
+
+  test("clonefile sync ignores link when no-follow: true", async () => {
+    const tempDir = directory();
+    const source = join(__dirname, "inputs/mario-link.txt");
+    const target = join(tempDir, "mario-clone.txt");
+    const result = cloneFileSync(source, target, { noFollow: true });
+
+    const stat = await fs.lstat(target);
+
+    expect(existsSync(target)).toBe(false);
+    expect(stat.isSymbolicLink()).toBe(true);
   });
 });
